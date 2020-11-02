fc9bd394bb2aa097554bcf979b20f0e8d2b10779
Eeshaanee Shandilya <u3555223@connect.hku.hk>
2020-11-02 20:51:25 +0800

    minor changes in the settings

--- hotzone_config/settings.py
+++ hotzone_config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$duk!*j-@ej3wco7)n_a(r9!)j0uh+w24y#0g44e5d(g*h6#=!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['stormy-castle-55753.herokuapp.com', 'localhost', '127.0.0.1']]
+ALLOWED_HOSTS = ['stormy-castle-55753.herokuapp.com', 'localhost', '127.0.0.1', 'desktop-h11uagi']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,5 +122,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static'),
+]
